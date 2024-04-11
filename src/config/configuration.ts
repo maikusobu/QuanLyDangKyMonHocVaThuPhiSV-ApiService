c5317c420d179d472d19edb9695f63a15269779d
maikusobu <0939899928za.com@gmail.com>
2024-04-11 15:22:07 +0700

    feat: update deploy pipeline

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,9 +1,6 @@
 export default () => ({
   env: process.env.NODE_ENV,
   port: parseInt(process.env.PORT, 10) || 3000,
-  url:
-    process.env.NODE_ENV === "development"
-      ? process.env.URL_DEV
-      : process.env.URL_PROD,
+  url: "",
   jwt_access_secret: process.env.JWT_ACCESS_SECRET,
 });

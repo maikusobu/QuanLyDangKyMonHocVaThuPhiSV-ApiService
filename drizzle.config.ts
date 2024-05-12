83eb95dda302d3c634aedd16e3fa3edbb6f29a79
Nhat-Original <nhatoriginal@gmail.com>
2024-05-12 10:00:14 +0700

    chore: update drizzle kit, update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
 import type { Config } from "drizzle-kit";
 
 export default {
-  driver: "pg",
+  dialect: "postgresql",
   out: "./drizzle",
   schema: "./src/db/schema/*",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL,
   },
   verbose: true,
   strict: true,
